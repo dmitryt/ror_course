7ae56560f2edd1166f2dd959739ee634f717cb0b
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2013-05-24 09:21:34 +0300

    updated

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -5,12 +5,32 @@ module Codebreaker
 		end
 
 		def start(secret)
+			@secret = secret
 			@output.puts "Welcome to Codebreaker!"
 			@output.puts "Enter guess:"
 		end
 
-		def guess(secret)
-			@output.puts ''
+		def guess(guess)
+			@memo = {}
+			_guess = guess.slice(0, 4)
+			n_match = e_match = 0
+			(0..._guess.length).each do |index|
+				n_match += 1 if number_match?(_guess, index)
+				e_match += 1 if exact_match?(_guess, index)
+				@memo[_guess[index]] = true
+			end
+			@output.puts [n_match, e_match].join(':')
+		end
+
+		private
+
+		def exact_match?(guess, index)
+			@secret[index] == guess[index]
+		end
+
+		def number_match?(guess, index)
+			return false if @memo[guess[index]]
+			@secret.include?(guess[index])
 		end
 	end
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -21,8 +21,40 @@ module Codebreaker
 			context "with no matches" do
 				it "sends a mark with ''" do
 					game.start('1234')
-					output.should_receive(:puts).with('')
+					output.should_receive(:puts).with('0:0')
 					game.guess('5678')
+				end
+			end
+
+			context "with 1 number match" do
+				it "sends a mark with '1:0'" do
+					game.start('1234')
+					output.should_receive(:puts).with('1:0')
+					game.guess('2678')
+				end
+			end
+
+			context "with 1 exact match" do
+				it "sends a mark with '1:1'" do
+					game.start('1234')
+					output.should_receive(:puts).with('1:1')
+					game.guess('1678')
+				end
+			end
+
+			context "with 2 numbers match" do
+				it "sends a mark with '2:0'" do
+					game.start('1234')
+					output.should_receive(:puts).with('2:0')
+					game.guess('2378')
+				end
+			end
+
+			context "with 1 exact match and 1 number match" do
+				it "sends a mark with '2:1'" do
+					game.start('1234')
+					output.should_receive(:puts).with('2:1')
+					game.guess('2537')
 				end
 			end
 		end
